b3b180c67ac88f94e8391701a45f5bbbec1a25b9
davidcoats <david@getsafetycone.com>
2022-11-18 17:54:33 -0800

    Interim changes.

---
+++ source/R5T.T0152/Code/_Types/Classes/PackageReference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0152
+{
+    /// <summary>
+    /// Represents a NuGet package reference (for example, in a project file).
+    /// </summary>
+    [DataTypeMarker]
+    public class PackageReference
+    {
+        /// <summary>
+        /// Example: Microsoft.AspNetCore.Components.WebAssembly.Server.
+        /// </summary>
+        public string Identity { get; set; }
+
+        /// <summary>
+        /// Example: 6.0.11
+        /// </summary>
+        public Version Version { get; set; }
+    }
+}
